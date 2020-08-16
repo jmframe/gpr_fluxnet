b484731a2dc9409c874e9664f918c4fd44fba76b
Jonathan Frame <jmframe@crimson.ua.edu>
2020-08-16 17:45:58 -0400

    saving 30 day forecast time steps

--- setup_dir/save_da_30day_output.py
+++ setup_dir/save_da_30day_output.py
@@ -18,15 +18,30 @@ with open('output.da', 'r') as f:
     df.columns = ['year', 'day', 'hour', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6','x7','sm',
                   'x8','x9','x10','x11','x12','x13','x14','x15','x16','x17','x18','x19','x20','x21']
     df2 = df.loc[(df.year == ty) & (df.day >= tds) & (df.day <= tde), ['year','day','hour','sm']]
-    print(df2)
     
+with open('obs_full.txt', 'r') as f:
+    df = pd.read_csv(f, delim_whitespace=True, header=None)
+    df.columns = ['year', 'day', 'hour', 'x1', 'x2', 'obs', 'x3', 'x4']
+    df3 = df.loc[(df.year == ty) & (df.day >= tds) & (df.day <= tde), ['year','day','hour','obs']]
+   
+df2.insert(4, "obs", df3['obs'])
+
+days_ahead = []
+for i, row in enumerate(df2.iterrows()):
+    days_ahead.append(df2.iloc[i, 1] - tds)
+df2.insert(0, "day_forecast", days_ahead)
+
 ## Write out the 30 day 'forecast' to file.
 if tds == 0:
     df2.to_csv('30_day_summary.txt', sep=' ', header=False, index=False, index_label=None)
 else:
     with open('30_day_summary.txt', 'r') as f:
-        df = pd.read_csv(f, delim_whitespace=True, header=None)
-        df.append(df2)
+        df3 = pd.read_csv(f, delim_whitespace=True, header=None)
+        df3.columns = ['year','day','hour','sm', 'obs']
+        print(df3.shape)
+        df3 = df3.append(df2)
+        print(df3.shape)
+    df3.to_csv('30_day_summary.txt', sep=' ', header=False, index=False, index_label=None)
 
 # Move the test start date one step forward
 with open('test_day_start.txt', 'w+') as f:
